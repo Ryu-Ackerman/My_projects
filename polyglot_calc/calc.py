dbe520fe2c7e09ea2c856a65734049724cc26307
Rimu <feruznasriddinov7@gmail.com>
2026-02-05 12:24:42 +0500

    better architecture

--- polyglot_calc/calc.py
+++ polyglot_calc/calc.py
@@ -1,22 +1,36 @@
-import sys
 import csv
+import sys
+
 
 
 class Holder():
-    def __init__(self, teacher, level, average=float):
+    def __init__(self, teacher, level, average):
         self.teacher = teacher
         self.level = level
-        try:
-            self.average = average
-        except ValueError as e:
-            raise e('The average should be a float!')
+        self.average = average
     def turn_to_dict(self):
         return {
             'teacher': self.teacher,
             'level': self.level,
             'average': self.average
         }
-
+    
+class Save():
+    def __init__(self, teacher, level, average):
+        self.teacher = teacher
+        self.level = level
+        self.average = average
+    def save_file(self):
+        fieldnames = ['teacher', 'level', 'average']
+        with open('end.csv', 'a') as f:
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+    
+LEVELS = {
+    'A2': 0.9,
+    'B1': 1/1.14,
+    'B1+': 0.84,
+    'B2': 0.64
+}
 
 def review_test():
     teacher = input('Enter the teacher name: ')
@@ -30,7 +44,6 @@ def review_test():
                 avg = round(sum(ans)/len(ans), 1)
                 with open('polyg.csv', 'a') as f:
                     writer = csv.DictWriter(f, fieldnames=fieldnames)
-                    # writer.writeheader()
                     hr = Holder(teacher, level, avg)
                     writer.writerow(hr.turn_to_dict())
                 sys.exit(f'The average is {avg}%')
@@ -43,11 +56,10 @@ def review_test():
             continue
 
 
-def end_year():
+
+def a1_end(level, teacher):
     fieldnames = ['teacher', 'level', 'average']
     lst = []
-    teacher = input('Enter the teacher name: ')
-    level = input('Enter the level: ').capitalize()
     if level == 'A1' or level == 'A1+':
         while True:
             correct_ans = input('Enter the number of correct ans or q for quit: ')
@@ -55,7 +67,6 @@ def end_year():
                 try:
                     with open('end.csv', 'a') as f:
                         writer = csv.DictWriter(f, fieldnames=fieldnames)
-                        writer.writeheader()
                         avg = round(sum(lst)/len(lst), 1)
                         hr = Holder(teacher, level, avg)
                         writer.writerow(hr.turn_to_dict())
@@ -76,15 +87,17 @@ def end_year():
                     continue
 
 
-    if level == 'A2':
-        while True:
+def handle_levels(level, teacher):
+    lst = []
+    fieldnames = ['teacher', 'level', 'average']
+    multiplier = LEVELS.get(level)
+    while True:
             reading = input('Enter the reading: ').lower()
             listening = input('Enter the listening: ').lower()
             if reading == 'q' or listening == 'q':
                 try:
                     with open('end.csv', 'a') as f:
                         writer = csv.DictWriter(f, fieldnames=fieldnames)
-                        writer.writeheader()
                         avg = round(sum(lst)/len(lst), 1)
                         hr = Holder(teacher, level, avg)
                         writer.writerow(hr.turn_to_dict())
@@ -97,127 +110,7 @@ def end_year():
                 except ValueError:
                     print('The last input was not an int!')
                     continue
-                pre_total = lr * 0.9
-                while True:
-                    writing = input('Enter the writing: ')                    
-                    speaking = input("Enter the speaking: ")
-                    if writing == 'q' or speaking == 'q':
-                        try:
-                            sys.exit(f'The class average is {avg}%')
-                        except ZeroDivisionError:
-                            sys.exit('No data entered!')
-                    try:
-                        total = pre_total + int(writing) + int(speaking)
-                        lst.append(total)
-                        print(f"{round(total, 1)}%")
-                        break
-                    except ValueError:
-                        print('The last input was not an int!')
-                        continue
-    
-
-    if level == 'B1':
-        while True:
-            reading = input('Enter the reading: ').lower()
-            listening = input('Enter the listening: ').lower()
-            if reading == 'q' or listening == 'q':
-                try:
-                    with open('end.csv', 'a') as f:
-                        writer = csv.DictWriter(f, fieldnames=fieldnames)
-                        writer.writeheader()
-                        avg = round(sum(lst)/len(lst), 1)
-                        hr = Holder(teacher, level, avg)
-                        writer.writerow(hr.turn_to_dict())
-                    sys.exit(f"The class avg {avg}%")
-                except ZeroDivisionError:
-                    sys.exit('No data entered!')
-            else:
-                try:
-                    lr = int(reading) + int(listening)
-                except ValueError:
-                    print('The last input was not an int!')
-                    continue
-                pre_total = lr / 1.14
-                while True:
-                    writing = input('Enter the writing: ')                    
-                    speaking = input("Enter the speaking: ")
-                    if writing == 'q' or speaking == 'q':
-                        try:
-                            sys.exit(f'The class average is {avg}%')
-                        except ZeroDivisionError:
-                            sys.exit('No data entered!')
-                    try:
-                        total = pre_total + int(writing) + int(speaking)
-                        lst.append(total)
-                        print(f"{round(total, 1)}%")
-                        break
-                    except ValueError:
-                        print('The last input was not an int!')
-                        continue
-    
-
-    if level == 'B1+':
-        while True:
-            reading = input('Enter the reading: ').lower()
-            listening = input('Enter the listening: ').lower()
-            if reading == 'q' or listening == 'q':
-                try:
-                    with open('end.csv', 'a') as f:
-                        writer = csv.DictWriter(f, fieldnames=fieldnames)
-                        writer.writeheader()
-                        avg = round(sum(lst)/len(lst), 1)
-                        hr = Holder(teacher, level, avg)
-                        writer.writerow(hr.turn_to_dict())
-                    sys.exit(f"The class avg {avg}%")
-                except ZeroDivisionError:
-                    sys.exit('No data entered!')
-            else:
-                try:
-                    lr = int(reading) + int(listening)
-                except ValueError:
-                    print('The last input was not an int!')
-                    continue
-                pre_total = lr * 0.84
-                while True:
-                    writing = input('Enter the writing: ')                    
-                    speaking = input("Enter the speaking: ")
-                    if writing == 'q' or speaking == 'q':
-                        try:
-                            sys.exit(f'The class average is {avg}%')
-                        except ZeroDivisionError:
-                            sys.exit('No data entered!')
-                    try:
-                        total = pre_total + int(writing) + int(speaking)
-                        lst.append(total)
-                        print(f"{round(total, 1)}%")
-                        break
-                    except ValueError:
-                        print('The last input was not an int!')
-                        continue
-    
-                    
-    if level == 'B2':
-        while True:
-            reading = input('Enter the reading: ').lower()
-            listening = input('Enter the listening: ').lower()
-            if reading == 'q' or listening == 'q':
-                try:
-                    with open('end.csv', 'a') as f:
-                        writer = csv.DictWriter(f, fieldnames=fieldnames)
-                        writer.writeheader()
-                        avg = round(sum(lst)/len(lst), 1)
-                        hr = Holder(teacher, level, avg)
-                        writer.writerow(hr.turn_to_dict())
-                    sys.exit(f"The class avg {avg}%")
-                except ZeroDivisionError:
-                    sys.exit('No data entered!')
-            else:
-                try:
-                    lr = int(reading) + int(listening)
-                except ValueError:
-                    print('The last input was not an int!')
-                    continue
-                pre_total = lr * 0.64
+                pre_total = lr * multiplier
                 while True:
                     writing = input('Enter the writing: ')                    
                     speaking = input("Enter the speaking: ")
@@ -235,24 +128,33 @@ def end_year():
                         print('The last input was not an int!')
                         continue
 
+
+def end_of_year():
+    teacher = input("Enter the teacher's name: ")
+    level = input('Enter the level: ').capitalize()
+    if level in LEVELS:
+        handle_levels(level, teacher)
+    elif level in ('A1', 'A1+'):
+        a1_end(level, teacher)
+    else:
+        sys.exit('Uknown level!')
+
 dict_ = {
+    'end': end_of_year,
     'review': review_test,
-    'end': end_year,
     '-h': lambda : [print("*",i) for i in dict_.keys()]
 }
 
-
 def main():
     if len(sys.argv) < 2:
-        sys.exit('Exit for now later fix to something better, -h for help!')
+        sys.exit('No enough arguments on the terminal!' \
+        '-h for help')
+    command = dict_.get(sys.argv[1].lower())
+    if command:
+        command()
     else:
-        command = dict_.get(sys.argv[1].lower())
-        if command:
-            command()
-        else:
-            print('Command not found, -h for help')
+        sys.exit('Uknown command!' \
+        '-h for help')
 
-#level control could be improved instead of using if and if and if I think this could be improved somehow
-#the project is not fully complete yet
 if __name__ == '__main__':
     main()
